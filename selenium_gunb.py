065d7cb35bba240ac75d26c8c59a6e5775316c14
Piotr Kubon <piotr.kubon@capgemini.com>
2024-07-20 04:38:38 +0200

    ocr

--- selenium_gunb.py
+++ selenium_gunb.py
@@ -1,7 +1,13 @@
+import os
+import easyocr
+
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
+
+from XPathModel1 import XPathModel1
+from data_model import DataModel
 
 url_start = "https://wyszukiwarka.gunb.gov.pl/"
 
@@ -27,11 +33,111 @@ def wait_for_element(driver: WebDriver, x_path: str, max_wait_time_s: float):
         time.sleep(1 / 100)
     return 1
 
+def insert_requested_values(driver: WebDriver, data_model:DataModel):
+    xpath1_model = data_model.xpath_model_1
+    x_path_vals = XPathModel1()
+
+    # if xpath1_model.rodzaj_wyszukiwania != "":
+    #     wait_for_element(driver, x_path_vals.rodzaj_wyszukiwania, 10)
+    #     driver.find_element(By.XPATH, x_path_vals.rodzaj_wyszukiwania).click()
+    #     time.sleep(1)
+    # if xpath1_model.typ_dokumentu != "":
+    #     wait_for_element(driver, x_path_vals.typ_dokumentu, 10)
+    #     driver.find_element(By.XPATH, x_path_vals.typ_dokumentu).click()
+    #     time.sleep(1)
+    if xpath1_model.numer_ewidencyjny_wniosku != "":
+        wait_for_element(driver, x_path_vals.numer_ewidencyjny_wniosku, 10)
+        driver.find_element(By.XPATH, x_path_vals.numer_ewidencyjny_wniosku).send_keys(xpath1_model.numer_ewidencyjny_wniosku)
+        time.sleep(1)
+    if xpath1_model.numer_ewidencyjny_decyzji != "":
+        wait_for_element(driver, x_path_vals.numer_ewidencyjny_decyzji, 10)
+        driver.find_element(By.XPATH, x_path_vals.numer_ewidencyjny_decyzji).send_keys(xpath1_model.numer_ewidencyjny_decyzji)
+        time.sleep(1)
+    if xpath1_model.data_zlozenia_wniosku_od != "":
+        wait_for_element(driver, x_path_vals.data_zlozenia_wniosku_od, 10)
+        driver.find_element(By.XPATH, x_path_vals.data_zlozenia_wniosku_od).send_keys(xpath1_model.data_zlozenia_wniosku_od)
+        time.sleep(1)
+    if xpath1_model.data_zlozenia_wniosku_do != "":
+        wait_for_element(driver, x_path_vals.data_zlozenia_wniosku_do, 10)
+        driver.find_element(By.XPATH, x_path_vals.data_zlozenia_wniosku_do).send_keys(xpath1_model.data_zlozenia_wniosku_do)
+        time.sleep(1)
+    if xpath1_model.data_wydania_decyzji_od != "":
+        wait_for_element(driver, x_path_vals.data_wydania_decyzji_od, 10)
+        driver.find_element(By.XPATH, x_path_vals.data_wydania_decyzji_od).send_keys(xpath1_model.data_wydania_decyzji_od)
+        time.sleep(1)
+    if xpath1_model.data_wydania_decyzji_do != "":
+        wait_for_element(driver, x_path_vals.data_wydania_decyzji_do, 10)
+        driver.find_element(By.XPATH, x_path_vals.data_wydania_decyzji_do).send_keys(xpath1_model.data_wydania_decyzji_do)
+        time.sleep(1)
+    # if xpath1_model.wojewodztwo_1 != "":
+    #     wait_for_element(driver, x_path_vals.wojewodztwo_1, 10)
+    #     driver.find_element(By.XPATH, x_path_vals.wojewodztwo_1).click()
+    #     time.sleep(1)
+    # if xpath1_model.organ_administracji != "":
+    #     wait_for_element(driver, x_path_vals.organ_administracji, 10)
+    #     driver.find_element(By.XPATH, x_path_vals.organ_administracji).click()
+    #     time.sleep(1)
+    # if xpath1_model.wojewodztwo_2 != "":
+    #     wait_for_element(driver, x_path_vals.wojewodztwo_2, 10)
+    #     driver.find_element(By.XPATH, x_path_vals.wojewodztwo_2).click()
+    #     time.sleep(1)
+    if xpath1_model.miejscowosc != "":
+        wait_for_element(driver, x_path_vals.miejscowosc, 10)
+        driver.find_element(By.XPATH, x_path_vals.miejscowosc).send_keys(xpath1_model.miejscowosc)
+        time.sleep(1)
+    if xpath1_model.ulica != "":
+        wait_for_element(driver, x_path_vals.ulica, 10)
+        driver.find_element(By.XPATH, x_path_vals.ulica).send_keys(xpath1_model.ulica)
+        time.sleep(1)
+    if xpath1_model.numer_domu != "":
+        wait_for_element(driver, x_path_vals.numer_domu, 10)
+        driver.find_element(By.XPATH, x_path_vals.numer_domu).send_keys(xpath1_model.numer_domu)
+        time.sleep(1)
+    if xpath1_model.nr_dzialki_ew != "":
+        wait_for_element(driver, x_path_vals.nr_dzialki_ew, 10)
+        driver.find_element(By.XPATH, x_path_vals.nr_dzialki_ew).send_keys(xpath1_model.nr_dzialki_ew)
+        time.sleep(1)
+    if xpath1_model.obreb_ew != "":
+        wait_for_element(driver, x_path_vals.obreb_ew, 10)
+        driver.find_element(By.XPATH, x_path_vals.obreb_ew).send_keys(xpath1_model.obreb_ew)
+        time.sleep(1)
+    if xpath1_model.arkusz_mapy != "":
+        wait_for_element(driver, x_path_vals.arkusz_mapy, 10)
+        driver.find_element(By.XPATH, x_path_vals.arkusz_mapy).send_keys(xpath1_model.arkusz_mapy)
+        time.sleep(1)
+    # if xpath1_model.rodzaj_zamierzenia_budowlanego != "":
+    #     wait_for_element(driver, x_path_vals.rodzaj_zamierzenia_budowlanego, 10)
+    #     driver.find_element(By.XPATH, x_path_vals.rodzaj_zamierzenia_budowlanego).click()
+    #     time.sleep(1)
+    # if xpath1_model.kategoria_obiektu != "":
+    #     wait_for_element(driver, x_path_vals.kategoria_obiektu, 10)
+    #     driver.find_element(By.XPATH, x_path_vals.kategoria_obiektu).click()
+    #     time.sleep(1)
+    if xpath1_model.nazwa_zamierzenia != "":
+        wait_for_element(driver, x_path_vals.nazwa_zamierzenia, 10)
+        driver.find_element(By.XPATH, x_path_vals.nazwa_zamierzenia).send_keys(xpath1_model.nazwa_zamierzenia)
+        time.sleep(1)
+
+
+    wait_for_element(driver, x_path_vals.captcha_img_src, 10)
+    path = os.path.join(data_model.screenshot_location, "captcha_screen.png")
+    driver.find_element(By.XPATH, x_path_vals.captcha_img_src).screenshot(path)
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(path)[0]
+    wait_for_element(driver, x_path_vals.captcha_img_inp, 10)
+    driver.find_element(By.XPATH, x_path_vals.captcha_img_inp).send_keys(result[1])
+    time.sleep(1)
+    wait_for_element(driver, x_path_vals.search, 10)
+    driver.find_element(By.XPATH, x_path_vals.search).click()
+    time.sleep(1)
 
 def main():
+    model = DataModel()
+
     driver = get_init_driver()
-    driver.get(url_start)
-    time.sleep(1)
+    driver.get(model.xpath_model_1.url)
+    time.sleep(5)
+    insert_requested_values(driver, model)
 
 
 if __name__ == "__main__":
