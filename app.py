8d43e272cf330f7e5f56c7cbbb5d5ee881859492
piotex <pkubon2@gmail.com>
2024-08-08 20:17:20 +0200

    a1/2 working v2

--- app.py
+++ app.py
@@ -5,10 +5,17 @@ from flask import Flask, redirect, url_for, request, render_template
 import webbrowser
 from threading import Timer
 
-from utils_app import filter_data, app_delete_filter, app_add_filter
+from utils_app import filter_data, app_delete_filter, app_add_filter, app_add_header_filter
 
 app = Flask(__name__)
 
+
+
+
+@app.route('/add_header_filter')
+def add_header_filter():
+    app_add_header_filter(request.args)
+    return generate_report()
 
 @app.route('/add_filter')
 def add_filter():
@@ -26,53 +33,54 @@ def delete_filter():
 def generate_report():
     with open('filters.json', encoding='utf-8') as f:
         filters = json.load(f)
+
+    header_filters = filters["header_filters"]
+    logical_filters = filters["logical_filters"]
+
     with open('result.json', encoding='utf-8') as f:
         gunb_data = json.load(f)
 
-    keyword_categories = [key for key in gunb_data[0]]
+    table_headers = [key for key in gunb_data[0]]
 
-    gunb_data = filter_data(gunb_data, filters)
+    gunb_data = filter_data(gunb_data, logical_filters, header_filters)
 
-    # if len(gunb_data) < 1:
-    #     gunb_data = [{}]
-    #     for key in keyword_categories:
-    #         gunb_data[0][key] = ""
 
-    table_headers = keyword_categories
-
-    return render_template("index.html", view="generate_report.html",
+    return render_template("index.html",
+                           view="generate_report.html",
                            table_headers=table_headers,
-                           table_items=gunb_data, old_filters_keywords=filters)
+                           header_filters=header_filters,
+                           logical_filters=logical_filters,
+                           table_items=gunb_data)
 
 
 @app.route('/download_data')
 def download_data():
     import gspread
-    email = ""
-    gc = gspread.service_account(filename='../secrets/gunb-bot-project-credentials.json')
-    sh = gc.create("gunb-sierpien")
-    spreadsheet_url = "https://docs.google.com/spreadsheets/d/%s" % sh.id
-
-    worksheet = sh.add_worksheet(title="gunb-worksheet-1", rows="100", cols="20")
-
-    with open('filters.json', encoding='utf-8') as f:
-        filters = json.load(f)
-    with open('result.json', encoding='utf-8') as f:
-        gunb_data = json.load(f)
-
-    keyword_categories = [key for key in gunb_data[0]]
-    for i, key in enumerate(keyword_categories, start=0):
-        worksheet.update_acell(f'{string.ascii_uppercase[i]}1', key)
-
-    gunb_data = filter_data(gunb_data, filters)
-    for i, item in enumerate(gunb_data, start=2):
-        for j, key in enumerate(item, start=0):
-            worksheet.update_acell(f'{string.ascii_uppercase[j]}{i}', item[key])
-
-    sh.share(email, perm_type='user', role='writer')
-    print(spreadsheet_url)
-    return redirect(spreadsheet_url, code=302)
-    return render_template("index.html", view="download_data.html")
+    # email = ""
+    # gc = gspread.service_account(filename='../secrets/gunb-bot-project-credentials.json')
+    # sh = gc.create("gunb-sierpien")
+    # spreadsheet_url = "https://docs.google.com/spreadsheets/d/%s" % sh.id
+    #
+    # worksheet = sh.add_worksheet(title="gunb-worksheet-1", rows="100", cols="20")
+    #
+    # with open('filters.json', encoding='utf-8') as f:
+    #     filters = json.load(f)
+    # with open('result.json', encoding='utf-8') as f:
+    #     gunb_data = json.load(f)
+    #
+    # keyword_categories = [key for key in gunb_data[0]]
+    # for i, key in enumerate(keyword_categories, start=0):
+    #     worksheet.update_acell(f'{string.ascii_uppercase[i]}1', key)
+    #
+    # gunb_data = filter_data(gunb_data, filters)
+    # for i, item in enumerate(gunb_data, start=2):
+    #     for j, key in enumerate(item, start=0):
+    #         worksheet.update_acell(f'{string.ascii_uppercase[j]}{i}', item[key])
+    #
+    # sh.share(email, perm_type='user', role='writer')
+    # print(spreadsheet_url)
+    # return redirect(spreadsheet_url, code=302)
+    # return render_template("index.html", view="download_data.html")
 
 
 @app.route('/home')
--- utils_app.py
+++ utils_app.py
@@ -1,43 +1,106 @@
 import json
 
 
-def filter_data(gunb_data, filters):
+def is_item_in_logical_filters(item, logical_filters):
+    for filter_keyword in logical_filters:
+        key = filter_keyword[0]
+        val = filter_keyword[1]
+        print(key)
+        if val not in item[key]:
+            return False
+    return True
+
+
+def get_item_with_header_filters_data(item, header_filters):
+    res = {}
+    for key in item:
+        if key in header_filters:
+            res[key] = item[key]
+    return res
+
+
+def filter_data(gunb_data, logical_filters, header_filters):
     data = []
     for item in gunb_data:
-        item_ok = True
-        for filter_keyword in filters:
-            val = filter_keyword[0]
-            key = filter_keyword[1]
-            if val not in item[key]:
-                item_ok = False
+        item_ok = is_item_in_logical_filters(item, logical_filters)
         if item_ok:
+            item = get_item_with_header_filters_data(item, header_filters)
             data.append(item)
     return data
 
 
 def app_delete_filter(args):
+    if "keyword_category_to_del" not in args:
+        return False
+    if "keyword_to_del" not in args:
+        return False
+
     with open('filters.json', encoding='utf-8') as f:
         filters = json.load(f)
 
-    if "keyword_category_to_del" in args and "keyword_to_del" in args:
-        res = []
-        for x in filters:
-            if args["keyword_to_del"] != x[0]:
-                res.append(x)
-        filters = res
+    header_filters = filters["header_filters"]
+    logical_filters = filters["logical_filters"]
+
+    res = []
+    for x in logical_filters:
+        if args["keyword_to_del"] != x[1] or args['keyword_category_to_del'] != x[0]:
+            res.append(x)
+    logical_filters = res
+
+    filters = {
+        "header_filters": header_filters,
+        "logical_filters": logical_filters
+    }
 
     with open('filters.json', 'w', encoding='utf-8') as f:
         json.dump(filters, f)
 
 
 def app_add_filter(args):
+    if "keyword_category" not in args:
+        return False
+    if "keyword" not in args:
+        return False
+
     with open('filters.json', encoding='utf-8') as f:
         filters = json.load(f)
 
-    if "keyword_category" in args and "keyword" in args:
-        keyword = args["keyword"]
-        keyword_category = args["keyword_category"].replace('\r', '')
-        filters.append([keyword, keyword_category])
+    header_filters = filters["header_filters"]
+    logical_filters = filters["logical_filters"]
+
+    keyword = args["keyword"]
+    keyword_category = args["keyword_category"].replace('\r', '')
+    logical_filters.append([keyword_category, keyword])
+
+    filters = {
+        "header_filters": header_filters,
+        "logical_filters": logical_filters
+    }
+
+    with open('filters.json', 'w', encoding='utf-8') as f:
+        json.dump(filters, f)
+
+
+def app_add_header_filter(args):
+    if "checkbox" not in args:
+        return False
+
+    with open('filters.json', encoding='utf-8') as f:
+        filters = json.load(f)
+
+    header_filters = filters["header_filters"]
+    logical_filters = filters["logical_filters"]
+
+    header_filters = []
+    for item in args:
+        if item == "checkbox":
+            continue
+        header_filters.append(item.replace("\r", ""))
+
+    filters = {
+        "header_filters": header_filters,
+        "logical_filters": logical_filters
+    }
 
     with open('filters.json', 'w', encoding='utf-8') as f:
         json.dump(filters, f)
