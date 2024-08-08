e9b9afc48abaebc69ddb1d2bd552dc8e4ef2a5e1
piotex <pkubon2@gmail.com>
2024-08-08 22:54:11 +0200

    a2/5 working v2

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 import json
-import string
-
-from flask import Flask, redirect, url_for, request, render_template
+import requests
 import webbrowser
 from threading import Timer
-
+from bs4 import BeautifulSoup
+from flask import Flask, redirect, url_for, request, render_template
 from utils_app import filter_data, app_delete_filter, app_add_filter, app_add_header_filter
 
 app = Flask(__name__)
-
-
 
 
 @app.route('/add_header_filter')
 def add_header_filter():
     app_add_header_filter(request.args)
     return generate_report()
+
 
 @app.route('/add_filter')
 def add_filter():
@@ -44,7 +42,6 @@ def generate_report():
 
     gunb_data = filter_data(gunb_data, logical_filters, header_filters)
 
-
     return render_template("index.html",
                            view="generate_report.html",
                            table_headers=table_headers,
@@ -53,9 +50,60 @@ def generate_report():
                            table_items=gunb_data)
 
 
+@app.route('/save_download_data')
+def save_download_data():
+    # with open('data.json', 'w', encoding='utf-8') as f:
+    #     json.dump(data, f)
+
+    pass
+
+
 @app.route('/download_data')
 def download_data():
-    import gspread
+    data_args = request.args
+
+    with open('posible_data_from_gunb.json', encoding='utf-8') as f:
+        posible_data = json.load(f)
+
+    if len(data_args) > 0:
+        checked_data = {}
+        for key in data_args:
+            if data_args[key] not in checked_data:
+                checked_data[data_args[key]] = []
+            checked_data[data_args[key]].append(key)
+
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(checked_data, f)
+
+    with open('data.json', encoding='utf-8') as f:
+        checked_data = json.load(f)
+
+
+
+    r = requests.get("https://wyszukiwarka.gunb.gov.pl/")
+    r.encoding = r.apparent_encoding
+    selects = BeautifulSoup(r.text).find_all('select')
+    options_wojewodztwo = {}
+    for op in selects[2].findAll('option'):
+        options_wojewodztwo[op.text.strip()] = op['value']
+    organ = {}
+    for woj in checked_data["Województwo"]:
+        organ[woj] = []
+        r = requests.get(f"https://wyszukiwarka.gunb.gov.pl/json/Search/getOrgsByRegion/?region={options_wojewodztwo[woj.lower()]}")
+        r.encoding = r.apparent_encoding
+        data_json = json.loads(r.text)
+        for item in data_json["data"]:
+            organ[woj].append(item["name"])
+
+
+
+
+    return render_template("index.html",
+                           view="download_data.html",
+                           checked_data=checked_data,
+                           posible_data=posible_data)
+
+    # import gspread
     # email = ""
     # gc = gspread.service_account(filename='../secrets/gunb-bot-project-credentials.json')
     # sh = gc.create("gunb-sierpien")
@@ -98,6 +146,24 @@ def open_browser():
 
 
 if __name__ == "__main__":
+    r = requests.get("https://wyszukiwarka.gunb.gov.pl/")
+    r.encoding = r.apparent_encoding
+    selects = BeautifulSoup(r.text).find_all('select')
+    options_typ_dokumentu = [op.text.strip() for op in selects[0].findAll('option')]
+    options_wojewodztwo = [op.text.strip() for op in selects[2].findAll('option')]
+    options_organ_administracji = [op.text.strip() for op in selects[3].findAll('option')]
+    options_rodzaj_zamierzenia_budowlanego = [op.text.strip() for op in selects[5].findAll('option')]
+    options_kategoria_obiektu = [op.text.strip() for op in selects[6].findAll('option')]
+    posible_data = {
+        "Typ dokumentu (Rejestr Wniosków i Decyzji)": options_typ_dokumentu,
+        "Rodzaj zamierzenia budowlanego": options_rodzaj_zamierzenia_budowlanego,
+        "Kategoria obiektu": options_kategoria_obiektu,
+        "Województwo": options_wojewodztwo,
+        "Organ administracji": options_organ_administracji,
+    }
+    with open('posible_data_from_gunb.json', 'w', encoding='utf-8') as f:
+        json.dump(posible_data, f)
+
     Timer(1, open_browser).start()
     app.run(port=5000)
 
