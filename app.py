3191236b8b9b34623fbfc890a1e72750fcd3100b
piotex <pkubon2@gmail.com>
2024-08-08 18:32:51 +0200

    working v2

--- app.py
+++ app.py
@@ -5,13 +5,48 @@ from flask import Flask, redirect, url_for, request, render_template
 import webbrowser
 from threading import Timer
 
-from utils_app import filter_data
+from utils_app import filter_data, app_delete_filter, app_add_filter
 
 app = Flask(__name__)
 
 
+@app.route('/add_filter')
+def add_filter():
+    app_add_filter(request.args)
+    return generate_report()
+
+
+@app.route('/delete_filter')
+def delete_filter():
+    app_delete_filter(request.args)
+    return generate_report()
+
+
 @app.route('/generate_report')
 def generate_report():
+    with open('filters.json', encoding='utf-8') as f:
+        filters = json.load(f)
+    with open('result.json', encoding='utf-8') as f:
+        gunb_data = json.load(f)
+
+    keyword_categories = [key for key in gunb_data[0]]
+
+    gunb_data = filter_data(gunb_data, filters)
+
+    # if len(gunb_data) < 1:
+    #     gunb_data = [{}]
+    #     for key in keyword_categories:
+    #         gunb_data[0][key] = ""
+
+    table_headers = keyword_categories
+
+    return render_template("index.html", view="generate_report.html",
+                           table_headers=table_headers,
+                           table_items=gunb_data, old_filters_keywords=filters)
+
+
+@app.route('/download_data')
+def download_data():
     import gspread
     email = ""
     gc = gspread.service_account(filename='../secrets/gunb-bot-project-credentials.json')
@@ -37,66 +72,17 @@ def generate_report():
     sh.share(email, perm_type='user', role='writer')
     print(spreadsheet_url)
     return redirect(spreadsheet_url, code=302)
+    return render_template("index.html", view="download_data.html")
 
 
-@app.route('/add_filter')
-def add_filter():
-    with open('filters.json', encoding='utf-8') as f:
-        filters = json.load(f)
-
-    args = request.args
-    if "keyword_category" in args and "keyword" in args:
-        keyword = args["keyword"]
-        keyword_category = args["keyword_category"].replace('\r','')
-        filters.append([keyword, keyword_category])
-
-    with open('filters.json', 'w', encoding='utf-8') as f:
-        json.dump(filters, f)
-
-    return filter()
-@app.route('/delete_filter')
-def delete_filter():
-    with open('filters.json', encoding='utf-8') as f:
-        filters = json.load(f)
-
-    args = request.args
-    if "keyword_category_to_del" in args and "keyword_to_del" in args:
-        res = []
-        for x in filters:
-            if args["keyword_to_del"] != x[0]:
-                res.append(x)
-        filters = res
-
-    with open('filters.json', 'w', encoding='utf-8') as f:
-        json.dump(filters, f)
-
-    return filter()
-
-
-@app.route('/filter')
-def filter():
-    args = request.args
-
-    with open('filters.json', encoding='utf-8') as f:
-        filters = json.load(f)
-    with open('result.json', encoding='utf-8') as f:
-        gunb_data = json.load(f)
-
-    keyword_categories = [key for key in gunb_data[0]]
-
-    gunb_data = filter_data(gunb_data, filters)
-
-    if len(gunb_data) < 1:
-        gunb_data = [{}]
-        for key in keyword_categories:
-            gunb_data[0][key] = ""
-
-    return render_template("filter.html", result=gunb_data, keyword_categories=keyword_categories, old_filters_keywords=filters)
+@app.route('/home')
+def home():
+    return index()
 
 
 @app.route('/')
 def index():
-    return filter()
+    return render_template("index.html", view="home.html")
 
 
 def open_browser():
--- utils_app.py
+++ utils_app.py
@@ -1,3 +1,6 @@
+import json
+
+
 def filter_data(gunb_data, filters):
     data = []
     for item in gunb_data:
@@ -10,3 +13,31 @@ def filter_data(gunb_data, filters):
         if item_ok:
             data.append(item)
     return data
+
+
+def app_delete_filter(args):
+    with open('filters.json', encoding='utf-8') as f:
+        filters = json.load(f)
+
+    if "keyword_category_to_del" in args and "keyword_to_del" in args:
+        res = []
+        for x in filters:
+            if args["keyword_to_del"] != x[0]:
+                res.append(x)
+        filters = res
+
+    with open('filters.json', 'w', encoding='utf-8') as f:
+        json.dump(filters, f)
+
+
+def app_add_filter(args):
+    with open('filters.json', encoding='utf-8') as f:
+        filters = json.load(f)
+
+    if "keyword_category" in args and "keyword" in args:
+        keyword = args["keyword"]
+        keyword_category = args["keyword_category"].replace('\r', '')
+        filters.append([keyword, keyword_category])
+
+    with open('filters.json', 'w', encoding='utf-8') as f:
+        json.dump(filters, f)
